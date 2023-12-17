b86e62b707eed2c81de74d08dc9c93f3030e5dd7
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2023-12-17 19:32:52 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -51,7 +51,7 @@ async def forward_to_channel(bot: Client, message: Message, editable: Message):
 
 async def save_batch_media_in_channel(bot: Client, editable: Message, message_ids: list):
     try:
-        message_ids_str = ""
+         message_ids_str = ""
         for message in (await bot.get_messages(chat_id=editable.chat.id, message_ids=message_ids)):
             sent_message = await forward_to_channel(bot, message, editable)
             if sent_message is None:
@@ -66,16 +66,14 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"get_short(https://t.me/{Config.BOT_USERNAME}?start=streaamdb_{str_to_b64(str(SaveMessage.id))})"
         short_link = get_short(share_link)
         await editable.edit(
-            f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {short_link} \n\n"
-            f"Just Click the link to get your files!",
+            f"**𝘉𝘢𝘵𝘤𝘩 𝘍𝘪𝘭𝘦𝘴 𝘚𝘵𝘰𝘳𝘦𝘥 𝘪𝘯 𝘮𝘺 𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦!**\n\n𝙃𝙚𝙧𝙚 𝙞𝙨 𝙩𝙝𝙚 𝙋𝙚𝙧𝙢𝙖𝙣𝙚𝙣𝙩 𝙇𝙞𝙣𝙠 𝙤𝙛 𝙮𝙤𝙪𝙧 𝙛𝙞𝙡𝙚𝙨: {short_link} \n\n"
+            f"𝘑𝘶𝘴𝘵 𝘊𝘭𝘪𝘤𝘬 𝘵𝘩𝘦 𝘭𝘪𝘯𝘬 𝘵𝘰 𝘨𝘦𝘵 𝘺𝘰𝘶𝘳 𝘧𝘪𝘭𝘦𝘴!",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Original Link", url=share_link),
-                  InlineKeyboardButton("Short Link", url=short_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
+                [[InlineKeyboardButton("⚡️ File Link ⚡️", url=short_link)],
+                 [InlineKeyboardButton("🔊How To Open Link", url="https://t.me/streaamdb/1578")]]
             ),
             disable_web_page_preview=True
         )
@@ -83,11 +81,66 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Original Link", url=short_link),
-                                                InlineKeyboardButton("Short Link", url=share_link)]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link)]])
         )
+        # ✧ Bina soche smjhe code edit mt krna wrna error dhundne mei umrr beet jaayega.
+        # ✧ source code upgraded by The sir LazyDeveloper 
+        # ✧ Don't remove credit ✧ @LazyDeveloper ✧
+        if(Config.LAZY_MODE == True):
+            thumbs= message.video.thumbs[0]
+            file_id= thumbs.file_id
+            lazy_channel = int(Config.LAZY_CHANNEL)
+            location=await bot.download_media(file_id)
+            lazypost_channel_username = (Config.LP_CHANNEL_USRNM)
+            lazypost_ch_admin_usrnm = (Config.LPCH_ADMIN_USRMN)
+            main_channel_username = (Config.LP_BTN_MAIN_CH_USRNM)
+            main_btn_link = f"https://telegram.me/{main_channel_username}"
+            file_name = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(message.caption))
+            
+            caption_z = f"{file_name}\n\n༺ᴊᴏɪɴ @{lazypost_channel_username} ༻\n\n🦋・‥☆𝘼𝘿𝙈𝙞𝙉 𝙨𝙪𝙥𝙥𝙤𝙧𝙩☆‥・🦋\n╰┈➤・☆ @{lazypost_ch_admin_usrnm} \n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_za = f"{file_name}\n\n༺ᴊᴏɪɴ @{lazypost_channel_username} ༻\n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_zab = f"{file_name}\n\n🦋・‥☆𝘼𝘿𝙈𝙞𝙉 𝙨𝙪𝙥𝙥𝙤𝙧𝙩☆‥・🦋\n╰┈➤・☆ @{lazypost_ch_admin_usrnm} \n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            caption_zabi = f"{file_name}\n\n+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            lazy_dev = f"+> ᴛʜᴀɴᴋ ʏᴏᴜ <a href='https://telegram.me/LazyDeveloper'>⎝⎝✧ʟᴀᴢʏᴅᴇᴠᴇʟᴏᴘᴇʀ✧⎠⎠</a>"
+            lazypost_custom_template = f"{(Config.LP_CUSTOM_TEMPLATE)}\n\n{lazy_dev} ♥️"
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("ミ★ GΞΓ FILΞ ★彡", url=short_link)],
+                 [InlineKeyboardButton("🔊How To Open Link", url="https://t.me/streaamdb/1578")],
+                ]
+            )
+            main_btn=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("ミ★ GΞΓ FILΞ ★彡", url=short_link)],
+                 [InlineKeyboardButton("🔊How To Open Link", url="https://t.me/streaamdb/1578")],
+                 [InlineKeyboardButton("•⊹٭𝚓𝚘𝚒𝚗 𝚖𝚊𝚒𝚗 𝚌𝚑𝚊𝚗𝚗𝚎𝚕٭⊹•", url=main_btn_link)]
+                ]
+            )
+            # ✧ Here is the condition for sending POST in movie channel
+            if(Config.LP_CUSTOM_TEMPLATE):
+                await bot.send_photo(lazy_channel,photo=location,caption=lazypost_custom_template,reply_markup=reply_markup)
+            elif(Config.LP_CUSTOM_TEMPLATE and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=lazypost_custom_template,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM and Config.LPCH_ADMIN_USRMN and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_z,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM and Config.LPCH_ADMIN_USRMN):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_z,reply_markup=reply_markup)
+            elif(Config.LP_CHANNEL_USRNM and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_za,reply_markup=main_btn)
+            elif(Config.LP_CHANNEL_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_za,reply_markup=reply_markup)
+            elif(Config.LPCH_ADMIN_USRMN and Config.LP_BTN_MAIN_CH_USRNM):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zab,reply_markup=main_btn)
+            elif(Config.LPCH_ADMIN_USRMN):
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zab,reply_markup=reply_markup)
+            else:
+                await bot.send_photo(lazy_channel,photo=location,caption=caption_zabi,reply_markup=reply_markup)
+                # ✧ Please don't add unnescesary things here >[LazyDeveloper]
+                cptz = f"🥷\n\nღ♡ **𝘗𝘖𝘚𝘛 𝘜𝘗𝘓𝘖𝘈𝘋𝘌𝘋 𝘖𝘕 𝘊𝘏𝘈𝘕𝘕𝘌𝘓 𝘚𝘜𝘊𝘊𝘌𝘚𝘚𝘍𝘜𝘓𝘓𝘠**✅\n\n**• NOTE: **ᴘʟᴇᴀꜱᴇ ᴏɴʟʏ ᴄʟɪᴄᴋ ->** ɢᴇᴛ ʙᴀᴛᴄʜ & ᴘᴏꜱᴛ **<- ᴏɴ ᴀɴʏ ᴏɴᴇ ᴏꜰ ᴛʜᴇ ᴀʙᴏᴠᴇ ᴍᴇꜱꜱᴀɢᴇꜱ\n\nN͢O͢ o͢f͢ c͢l͢i͢c͢k͢ = n͢o͢ o͢f͢ p͢o͢s͢t͢s͢\n\n• ʙᴇ ᴄᴀʀᴇꜰᴜʟʟ - ʏᴏᴜ ᴀʀᴇ ᴄᴜʀʀᴇɴᴛʟʏ ɪɴ ʟᴀᴢʏ_ᴍᴏᴅᴇ\n• @streaamdb"
+                k = await message.reply_text(text=cptz)
+                await asyncio.sleep(30)
+                await k.delete()
+        
     except Exception as err:
-        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
+        await editable.edit(f"ꜱᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...\n\n**Error:** `{err}`")
         await bot.send_message(
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
@@ -97,7 +150,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                     [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
                 ]
             )
-        )
+            )
 
 
 async def save_media_in_channel(bot: Client, editable: Message, message: Message):
